88636bef6e8b769d14ec4e9dba9f6d6445f72434
Nikhil-Jalandhra <jneek36@gmail.com>
2025-07-28 09:45:06 +0530

    update outOfBox page content

--- src/app/outofbox/page.tsx
+++ src/app/outofbox/page.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import "./page.css"
+import "./page.css";
+import { TfiThought } from "react-icons/tfi";
 import MainLayout from '../components/(MainLayout)/MainLayout';
 
-function page() {
+function Page() {
   return (
     <MainLayout>
       <div className='outOfBoxContainer'>
-        hlo;
+        <span className='outOfBoxThtIcn'>
+          <TfiThought />
+        </span>
+        <h1><span className='outOfBoxAwait'>await</span> <span className='outOfBoxContent'>content</span>( );</h1>
+        <p>// Still loading the future...</p>
       </div>
     </MainLayout>
   );
 }
 
-export default page;
+export default Page;
